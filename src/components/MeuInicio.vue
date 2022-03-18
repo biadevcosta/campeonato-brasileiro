95e5ce22b2f28e19bc9738ab9afdfddb5a16ce3d
Ana Beatriz <contato.anacosta2@gmail.com>
2022-03-18 00:03:53 -0300

    tela de inicio

--- src/components/MeuInicio.vue
+++ src/components/MeuInicio.vue
@@ -1,6 +1,11 @@
 <template>
     <v-container>
-        <h1>oi</h1>
+        <h2 class="text-h5 text-center mb-3 mt-5">Brasileirão Série A</h2>
+        <v-img class="inicio-imagem mb-5" 
+         src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png"> 
+         </v-img>
+         <p class="text-justify">A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente Brasileirão Assaí – Série A 2020 por motivos de patrocínio, é a 64.ª edição da principal divisão do futebol brasileiro. A disputa tem o mesmo regulamento dos anos anteriores, quando foi implementado o sistema de pontos corridos.
+         </p>
     </v-container>
 </template>
 
@@ -11,5 +16,7 @@ export default {
 </script>
 
 <style scooped>
-
+    .inicio-imagem{
+        border-radius: 5px
+    }
 </style>
