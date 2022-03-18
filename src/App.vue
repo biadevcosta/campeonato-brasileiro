ad2fc76702aef33258f136ce320a90e36a3990e8
Ana Beatriz <contato.anacosta2@gmail.com>
2022-03-18 00:44:06 -0300

    pasta shared

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar app
-      color="#66BB6A"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <AppTopBar/>
 
-      <v-toolbar-title>Campeonato Brasileiro 2020 ⚽</v-toolbar-title>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list 
-        nav 
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="green--text text--lighten-2"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Início</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-soccer</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Classificação</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
-      <v-container>
-          <MeuInicio/>
-         </v-container>
+        <MeuInicio/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+
+import AppTopBar from './components/shared/AppTopBar.vue';
 import MeuInicio from './components/MeuInicio.vue';
+
 
 export default {
   name: 'App',
-
   components: {
     MeuInicio,
+    AppTopBar,
   },
-
-  data: () => ({
-    drawer: false,
-    group: null,
-  }),
 };
 </script>
+
+
+<style>
+
+</style>
